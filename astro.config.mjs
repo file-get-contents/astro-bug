b2ece9aab79c4db22f071a959ae401ef704e8a2b
file-get-contents <36499606+file-get-contents@users.noreply.github.com>
2026-06-06 15:30:15 +0900

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import node from '@astrojs/node'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [react()],
+    server: {
+        port: 8080,
+        host: true,
+    },
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
+})
---
+++ src/components/test.tsx
@@ -0,0 +1,17 @@
+export default function () {
+
+    return(
+
+        <html lang="ja">
+            <head>
+                <meta charSet="UTF-8" />
+                <title>Test</title>
+                <link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+            </head>
+            <body>
+                <h1>Test</h1>
+            </body>
+        </html>
+    )
+
+}
